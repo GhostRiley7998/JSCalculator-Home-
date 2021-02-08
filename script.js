7390a5d71d77fee9ff8a6a865df40b840090cc8e
Hung7998 <76940216+Hung7998@users.noreply.github.com>
2021-02-08 08:27:20 +0700

    Máy tính đầy đủ

--- script.js
+++ script.js
@@ -1,1 +1,51 @@
-document.getElementById("screen").value = "123456"
+function Plus() {
+  document.getElementById("screen").value = "+";
+}
+function Subtract() {
+  document.getElementById("screen").value = "-";
+}
+function Multiply() {
+  document.getElementById("screen").value = "×";
+}
+function Divide() {
+  document.getElementById("screen").value = "÷";
+}
+function seven() {
+    document.getElementById("screen").value = "7";
+}
+function eight() {
+    document.getElementById("screen").value = "8";
+}
+function nine() {
+    document.getElementById("screen").value = "9";
+}
+function four() {
+    document.getElementById("screen").value = "4";
+}
+function five() {
+    document.getElementById("screen").value = "5";
+}
+function six() {
+    document.getElementById("screen").value = "6";
+}
+function one() {
+    document.getElementById("screen").value = "1";
+}
+function two() {
+    document.getElementById("screen").value = "2";
+}
+function three() {
+    document.getElementById("screen").value = "3";
+}
+function decimal() {
+    document.getElementById("screen").value = ".";
+}
+function zero() {
+    document.getElementById("screen").value = "0";
+}
+function ac() {
+    document.getElementById("screen").value = "_";
+}
+function calc() {
+    document.getElementById("screen").value = "nothing yet";
+}
